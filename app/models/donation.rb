68466b04797c33971f1202051e02934a6026fc0f
Tobias Talltorp <tobias@talltorp.se>
2015-12-10 16:34:21 +0100

    Validate format for email address

--- app/models/donation.rb
+++ app/models/donation.rb
@@ -1,5 +1,6 @@
 class Donation < ActiveRecord::Base
   validates_presence_of :name, :phone, :email, :donation_amount
+  validates_format_of :email, with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, on: :create
 
   belongs_to :campaign
 end
