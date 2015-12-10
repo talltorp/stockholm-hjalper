f53813662bbd26614dcf34d3da9631c7671de981
Tobias Talltorp <tobias@talltorp.se>
2015-12-10 13:25:52 +0100

    Calculate the total amount of donations made

--- app/models/campaign.rb
+++ app/models/campaign.rb
@@ -1,3 +1,7 @@
 class Campaign < ActiveRecord::Base
   has_many :donations
+
+  def total_donation_amount
+    donations.sum(:donation_amount)
+  end
 end
--- spec/models/campaign_spec.rb
+++ spec/models/campaign_spec.rb
@@ -1,5 +1,44 @@
 require 'rails_helper'
 
 RSpec.describe Campaign, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "#total_donation_amount" do
+    context "with no donations" do
+      it "defaults to 0" do
+        campaign = build(:campaign, funding_goal: 10)
+
+        total_donation_amount = campaign.total_donation_amount
+
+        expect(total_donation_amount).to eql(0)
+      end
+    end
+
+    context "with three donations which makes up less than the maximum amount" do
+      it "summarizes the donations" do
+        campaign = create(:campaign, funding_goal: 10)
+        campaign.donations << create(:donation, donation_amount: 2)
+        campaign.donations << create(:donation, donation_amount: 3)
+        campaign.donations << create(:donation, donation_amount: 1)
+        campaign.save
+
+        total_donation_amount = campaign.total_donation_amount
+
+        expect(total_donation_amount).to eql(6)
+      end
+    end
+
+    context "with donations which equal the maximum amount" do
+      it "summarizes the donations" do
+        campaign = create(:campaign, funding_goal: 10)
+        campaign.donations << create(:donation, donation_amount: 2)
+        campaign.donations << create(:donation, donation_amount: 3)
+        campaign.donations << create(:donation, donation_amount: 1)
+        campaign.donations << create(:donation, donation_amount: 4)
+        campaign.save
+
+        total_donation_amount = campaign.total_donation_amount
+
+        expect(total_donation_amount).to eql(10)
+      end
+    end
+  end
 end
