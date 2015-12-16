b7fe590c4b570de05eba78578b2ed5d0f8ad789a
Tobias Talltorp <tobias@talltorp.se>
2015-12-16 12:09:08 +0100

    Add funding doughnut chart to the index page

--- app/assets/javascripts/campaigns.js
+++ app/assets/javascripts/campaigns.js
@@ -1,14 +1,30 @@
 createFundingDoughnutForShowPage();
+createFundingDoughnutForIndexPage();
 
 function createFundingDoughnutForShowPage() {
   var campaignsShowChart = $("#campaignShowChart");
 
-  buildFundingDoughnut("campaignShowChart",
-      campaignsShowChart.data("funding-left"),
-      campaignsShowChart.data("funding-current"));
+  if(campaignsShowChart.length) {
+    buildFundingDoughnut("campaignShowChart",
+        campaignsShowChart.data("funding-left"),
+        campaignsShowChart.data("funding-current"));
+  }
+}
+
+function createFundingDoughnutForIndexPage() {
+  var gridItems = $(".campaign-grid-item");
+  console.log(gridItems);
+
+  gridItems.each(function(gridItem) {
+    var canvas = $(this).find("canvas");
+    buildFundingDoughnut("campaign-" + canvas.data("id"),
+        canvas.data("funding-left"),
+        canvas.data("funding-current"));
+  });
 }
 
 function buildFundingDoughnut(elementId, fundingLeft, currentFunding) {
+  console.log(elementId);
   var options = {
     animation: false,
     showScale: false,
