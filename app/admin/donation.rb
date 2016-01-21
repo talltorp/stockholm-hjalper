7f2449abc082738273d152b5787cc3bf322a9e62
Tobias Talltorp <tobias@talltorp.se>
2016-01-21 20:25:09 +0100

    Admin: Edit and change parameters for donation

--- app/admin/donation.rb
+++ app/admin/donation.rb
@@ -3,7 +3,9 @@ ActiveAdmin.register Donation do
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
-# permit_params :list, :of, :attributes, :on, :model
+permit_params :donation_amount, :name, :phone, :email, :message,
+              :requested_pickup_time, :accepted, :organisation,
+              :campaign_id, :confirmation_code
 #
 # or
 #
